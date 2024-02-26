45513c136e6ede468492edbe9212782d7512d8a5
Songha Im <dddtttt000@gmail.com>
2024-02-26 18:35:30 +0900

    :lipstick: Update : main style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import "./App.css"
-import bg_frame from "../src/assets/bg_frame.png"
+// import bg_frame from "../src/assets/bg_frame.png"
 
 function App() {
   return (
     <div className="bg-black w-full h-svh flex justify-center">
-      <div className="w-[450px] bg-white">
-        <img src={bg_frame} alt="" className="relative" />
-        <div className=" fixed left-28 top-32 ">
-          <div className="lavishly-yours-regular text-6xl">Wedding</div>
-          <div className="licorice-regular text-6xl">Wedding</div>
+      <div className="w-[400px] bg-white relative">
+        <div id="bg" className="flex justify-center">
+          <div className="mt-28 text-center">
+            <div className="text-xs mb-6 font-thin">YOU ARE INVITED TO THE</div>
+            <div className="licorice-regular text-7xl font- text-center">
+              Wedding
+            </div>
+            <div className=" text-2xl text-center lavishly-yours-regular mb-5">
+              -of-
+            </div>
+            <div className=" text-3xl  font-light mb-2">Lin</div>
+            <div className=" text-2xl text-center lavishly-yours-regular mb-2">
+              and
+            </div>
+            <div className=" text-3xl  font-light mb-4">Songha</div>
+            <div className=" mt-5 text-sm">
+              <div className=" font-thin">2024.10.26 (토)</div>
+              <div className=" font-thin">오후 12:40</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
