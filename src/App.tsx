9d772008954b343deb45a9337c36d620d4ec3ec6
Songha Im <dddtttt000@gmail.com>
2024-02-26 17:30:27 +0900

    :lipstick: Add : main style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import bg_frame from "../src/assets/bg_frame.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black w-full h-svh flex justify-center">
+      <div className="w-[450px] bg-white">
+        <img src={bg_frame} alt="" className="relative" />
+        <div className=" fixed left-28 top-32 ">
+          <div className="lavishly-yours-regular text-6xl">Wedding</div>
+          <div className="licorice-regular text-6xl">Wedding</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
