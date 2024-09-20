23d0d3ef0696fb97667ccc8a3936d3c6f3aea63b
Songha Im <dddtttt000@gmail.com>
2024-09-20 17:19:05 +0900

    :construction: WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import "./App.css"
-// import bg_frame from "../src/assets/bg_frame.png"
-// import Link from "next/link"
-// import middle from "./assets/middle.png"
+import {
+  Container as MapDiv,
+  NaverMap,
+  Marker,
+  useNavermaps,
+} from "react-naver-maps"
+
 const App = () => {
+  const navermaps = useNavermaps()
+
   return (
     <>
-      <div className="w-full p-2 flex justify-center text-center font-sans">
+      <div className="w-full p-2 flex justify-center text-center nanum-myeongjo-regular">
         <div className="border-gray-100  w-96">
-          <div className="font-serif mt-10 text-2xl text-[#0E13E5] font-semibold">
+          <div className=" mt-10 text-2xl text-[#0E13E5] gowun-batang-bold">
             10 / 26
           </div>
-          <div className="flex justify-center items-center my-6 gap-6 font-serif">
+          <div className="flex justify-center items-center my-6 gap-6 nanum-myeongjo-regular">
             <div className="text-2xl ">남세린</div>
             <div className="text-[#0E13E5] font-semibold">&</div>
             <div className=" text-2xl">임송하</div>
@@ -30,21 +36,21 @@ const App = () => {
       {/* <div className="mt-24 flex justify-center">
         <img alt="" src={middle} />
         </div> */}
-      <div className="p-2 text-center font-sans">
+      <div className="p-2 text-center nanum-myeongjo-regular">
         <div className="flex justify-center py-8">
           <div className="border-b-[#0E13E5] border border-t-0 w-72"></div>
         </div>
 
-        <div className="text-[#0E13E5] text-sm font-serif">INVITATION</div>
+        <div className="text-[#0E13E5] text-sm">INVITATION</div>
         <div className=" text-[#0E13E5] mt-2 font-semibold">
           소중한 분들을 초대합니다
         </div>
 
-        <div className="mt-12 space-y-2 font-serif">
+        <div className="mt-12 space-y-2">
           <div>예쁜 예감이 들었다.</div>
           <div>우리는 언제나 손을 잡고 있게 될 것이다.</div>
           <div className="pt-3 text-sm">- 이이체, 〈연인〉 中 -</div>
-          {/* <div className="py-8 font-light">-</div> */}
+
           <div className="py-8">우리의 설레는 첫걸음을 함께 빛내주세요.</div>
 
           <div className="space-y-2">
@@ -64,7 +70,7 @@ const App = () => {
             <div className="border-b-[#0E13E5] border border-t-0 w-72"></div>
           </div>
 
-          <div className="font-sans">
+          <div className="">
             <div>
               <div className="text-lg">2024. 10. 26</div>
               <div>토요일 낮 12시 40분</div>
@@ -80,23 +86,45 @@ const App = () => {
             <div className="border-b-[#0E13E5] border border-t-0 w-72"></div>
           </div>
           <div className="text-[#0E13E5] text-sm">LOCATION</div>
-          <div className=" text-[#0E13E5] font-semibold font-sans">
-            오시는 길
-          </div>
-          <div className="font-sans pt-6">
+          <div className=" text-[#0E13E5] font-semibold ">오시는 길</div>
+          <div className=" pt-6">
             <div>웨딩 스퀘어 4층, 그레이스 홀</div>
             <div className="text-sm mt-2">서울 광진구 광나루로56길 85</div>
             <div className="text-sm mt-4">TEL. 02-3424-7000</div>
-            <div id="map" className="w-full h-[400px]"></div>
+            <div
+              id="map"
+              className="p-6 h-[400px] w-[400] flex justify-center mt-8"
+            >
+              <MapDiv
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  marginTop: "-30px",
+                  display: "flex",
+                }}
+                className="border"
+              >
+                <NaverMap
+                  defaultCenter={
+                    new navermaps.LatLng(37.535725176732, 127.095692162256)
+                  }
+                  defaultZoom={16}
+                >
+                  <Marker
+                    defaultPosition={
+                      new navermaps.LatLng(37.535725176732, 127.095692162256)
+                    }
+                  />
+                </NaverMap>
+              </MapDiv>
+            </div>
           </div>
 
           <div className="flex justify-center py-8">
             <div className="border-b-[#0E13E5] border border-t-0 w-72"></div>
           </div>
           <div className="text-[#0E13E5] text-sm">ACCOUNT</div>
-          <div className=" text-[#0E13E5] font-semibold font-sans">
-            마음 전하실 곳
-          </div>
+          <div className=" text-[#0E13E5] font-semibold ">마음 전하실 곳</div>
           <div className="font-thin space-y-2 text-sm pt-8">
             <div>참석이 어려우신 분들을 위해</div>
             <div>계좌번호를 기재하였습니다.</div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import { NavermapsProvider } from "react-naver-maps"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <NavermapsProvider ncpClientId="a51ubs0ntl">
+      <App />
+    </NavermapsProvider>
   </React.StrictMode>
 )
 
